49f0addf4fe261683db40284aaa621a6189762b8
RapidTest25 <rapidtest25@users.noreply.github.com>
2026-06-02 15:53:09 +0700

    Fix missed connect events causing false timeout

--- app.js
+++ app.js
@@ -97,17 +97,16 @@ class TikTokIOConnection {
     this.uniqueId = uniqueId;
     this.options = options || {};
 
-    this.socket.off("tiktokConnected");
-    this.socket.off("tiktokDisconnected");
-    this.socket.off("connect_error");
-
-    this.socket.emit("setUniqueId", this.uniqueId, this.options);
-
     return new Promise((resolve, reject) => {
+      this.socket.off("tiktokConnected");
+      this.socket.off("tiktokDisconnected");
+      this.socket.off("connect_error");
+
       const cleanup = () => {
         this.socket.off("tiktokConnected", onConnected);
         this.socket.off("tiktokDisconnected", onDisconnected);
         this.socket.off("connect_error", onConnectError);
+        clearTimeout(timeoutId);
       };
 
       const onConnected = (state) => {
@@ -129,10 +128,12 @@ class TikTokIOConnection {
       this.socket.once("tiktokDisconnected", onDisconnected);
       this.socket.once("connect_error", onConnectError);
 
-      setTimeout(() => {
+      const timeoutId = setTimeout(() => {
         cleanup();
         reject("Connection Timeout");
       }, 15000);
+
+      this.socket.emit("setUniqueId", this.uniqueId, this.options);
     });
   }
 
